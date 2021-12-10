c97477d100eb03ec00d8411488796d312fa1e148
Erik Carron <jecarron@gmail.com>
2021-12-10 00:30:05 -0600

    Added day 2.

---
+++ day02/part1.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+
+let parseCommands = input => input.split("\r\n").map(parseCommand)
+
+let parseCommand = input => {
+    let match = input.match(/^(.+) (\d+)$/mi)
+
+    var command = {
+        action: match[1],
+        units: parseInt(match[2], 10)
+    }
+
+    return command
+}
+
+let processCommands = commands => {
+    var position = { horizontal: 0, depth: 0 }
+
+    for (var i = 0; i < commands.length; i++) {
+        let command = commands[i]
+        if (command.action == 'forward') {
+            position.horizontal += command.units
+        } else if (command.action == 'down') {
+            position.depth += command.units
+        } else if (command.action == 'up') {
+            position.depth -= command.units
+        }
+    }
+
+    return position
+}
+
+let commands = parseCommands(fs.readFileSync('input.txt').toString())
+let position = processCommands(commands)
+
+console.log(position)
+console.log(`Answer: ${position.horizontal * position.depth}`)
---
+++ day02/part2.js
@@ -0,0 +1,39 @@
+var fs = require('fs')
+
+let parseCommands = input => input.split("\r\n").map(parseCommand)
+
+let parseCommand = input => {
+    let match = input.match(/^(.+) (\d+)$/mi)
+
+    var command = {
+        action: match[1],
+        units: parseInt(match[2], 10)
+    }
+
+    return command
+}
+
+let processCommands = commands => {
+    var position = { horizontal: 0, depth: 0, aim: 0 }
+
+    for (var i = 0; i < commands.length; i++) {
+        let command = commands[i]
+        if (command.action == 'forward') {
+            position.horizontal += command.units
+            position.depth += position.aim * command.units
+        } else if (command.action == 'down') {
+            position.aim += command.units
+        } else if (command.action == 'up') {
+            position.aim -= command.units
+        }
+    }
+
+    return position
+}
+
+
+let commands = parseCommands(fs.readFileSync('input.txt').toString())
+let position = processCommands(commands)
+
+console.log(position)
+console.log(`Answer: ${position.horizontal * position.depth}`)
