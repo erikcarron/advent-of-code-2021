56adc3c759b5d34b4ab711b8298bd21fd644b821
Erik Carron <jecarron@gmail.com>
2021-12-11 20:20:39 -0600

    Added day 7.

---
+++ day07/part1.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+
+let parseInput = input => input.split(',').map(input => parseInt(input, 10))
+
+let calculateFuel = (from, to) => Math.abs(from - to)
+
+let minPosition = positions => Math.min(...positions)
+
+let maxPosition = positions => Math.max(...positions)
+
+let calculateLowestCostPosition = positions => {
+  let min = minPosition(positions)
+  let max = maxPosition(positions)
+
+  let fuelCalculations = new Array(max)
+
+  for (var i = min; i <= max; i++) {
+    fuelCalculations[i] = positions.reduce((p, c) => p + calculateFuel(c, i), 0)
+  }
+
+  let minCost = Math.min(...fuelCalculations)
+  let minCostPosition = fuelCalculations.indexOf(minCost)
+  return { minCost: minCost, postion: minCostPosition }
+}
+
+var positions = parseInput(fs.readFileSync('input.txt').toString())
+
+let lowestCostPosition = calculateLowestCostPosition(positions)
+console.log(`Answer: ${lowestCostPosition.minCost}`)
