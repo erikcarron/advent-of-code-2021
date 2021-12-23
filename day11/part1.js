07ee496df5dbcffae724a96fbff099282e56988f
Erik Carron <jecarron@gmail.com>
2021-12-22 21:09:51 -0600

    Added day 11.

---
+++ day11/part1.js
@@ -0,0 +1,81 @@
+let fs = require('fs');
+let colors = require('colors');
+
+let parseInput = input => input.match(/^.+$/gm).map(i => i.split('').map(n => parseInt(n, 10)))
+
+let printOctopuses = octopuses => {
+  octopuses.forEach(i => console.log(i.map(n => n == 0 ? colors.brightGreen(n.toString()) : n.toString()).join('')))
+  console.log()
+}
+
+let increaseEnergyLevels = octopuses => {
+  for (var y = 0; y < octopuses.length; y++) {
+    for (var x = 0; x < octopuses[y].length; x++) {
+      octopuses[y][x]++
+    }
+  }
+}
+
+let executeFlash = octopuses => {
+  var flashCount = 0
+
+  while (anyReadyToFlash(octopuses)) {
+    for (var y = 0; y < octopuses.length; y++) {
+      for (var x = 0; x < octopuses[y].length; x++) {
+        if (readyToFlash(octopuses[y][x])) {
+          flashCount++
+          octopuses[y][x] = 0
+          increaseAdjacent(octopuses, x, y)
+        }
+      }
+    }
+  }
+
+  return flashCount
+}
+
+let increaseAdjacent = (octopuses, x, y) => {
+  let width = octopuses[0].length
+  let height = octopuses.length
+
+  for (var yOffset = -1; yOffset <= 1; yOffset++) {
+    for (var xOffset = -1; xOffset <= 1; xOffset++) {
+      let adjacentX = x + xOffset
+      let adjacentY = y + yOffset
+
+      if (adjacentX >= 0 && adjacentX < width && adjacentY >= 0 && adjacentY < height && (adjacentX != x || adjacentY != y) && !alreadyFlashed(octopuses[adjacentY][adjacentX])) {
+        octopuses[adjacentY][adjacentX]++
+      }
+    }
+  }
+}
+
+let readyToFlash = octopus => octopus > 9
+let alreadyFlashed = octopus => octopus == 0
+let anyReadyToFlash = octopuses => octopuses.some(y => y.some(x => readyToFlash(x)))
+
+let processStep = octopuses => {
+  increaseEnergyLevels(octopuses)
+  return executeFlash(octopuses)
+}
+
+let processSteps = (octopuses, stepCount) => {
+  var flashCount = 0;
+
+  for (var i = 0; i < stepCount; i++) {
+    flashCount += processStep(octopuses)
+    //printOctopuses(octopuses)
+  }
+
+  return flashCount
+}
+
+let octopuses = parseInput(fs.readFileSync('input.txt').toString())
+
+printOctopuses(octopuses)
+
+let answer = processSteps(octopuses, 100)
+
+printOctopuses(octopuses)
+
+console.log(`Answer: ${answer}`)
